96684838cc586442e1a5ce8adf57d7dae7f7ad62
Thais Duarte <49776289+Thash9523b@users.noreply.github.com>
2019-08-23 00:21:13 -0300

    Add files via upload

---
+++ Questao 5/ValorAbaixoException.h
@@ -0,0 +1,20 @@
+#ifndef VALORABAIXOEXCEPTION_H
+#define VALORABAIXOEXCEPTION_H
+
+#include <iostream>
+#include <exception>
+
+class ValorAbaixoException : public std::exception
+{
+    public:
+        ValorAbaixoException();
+        ~ValorAbaixoException();
+
+        virtual std::string what();
+
+    private:
+
+        std::string error = "Numero maior ou igual a zero.";
+};
+
+#endif // VALORABAIXOEXCEPTION_H
